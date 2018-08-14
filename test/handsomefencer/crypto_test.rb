4fbb91a83cf6a8426b18f6c73387b0e1f0008a39
schadenfred <fred.schoeneman@gmail.com>
2018-08-13 21:51:26 -0700

    tests passing

--- test/handsomefencer/crypto_test.rb
+++ test/handsomefencer/crypto_test.rb
@@ -4,55 +4,84 @@ describe Handsomefencer::Environment::Crypto do
 
   subject { Crypto.new }
 
-  Given(:file1) { '.env/circle.env' }
-  Given(:file2) { '.env/backup.env' }
-  Given(:file3) { '.env/development/backup.env' }
+  Given(:deploy_key) { 'config/deploy.key' }
 
-  Then { assert FileUtils.compare_file(file1, file2) }
-  And  { assert FileUtils.compare_file(file2, file3) }
+  describe "#save_deploy_key" do
 
-  describe "#generate_key" do
+    describe "with existing 'config/deploy.key'" do
 
-    Given { rmfile('config/deploy.key')}
-    Given { rmcode('.gitignore', '/config/deploy.key')}
-    Given { subject.generate_deploy_key }
+      Given { subject.save_deploy_key}
 
-    describe "generates config/deploy.key file" do
-
-      Then { assert File.exist? 'config/deploy.key'}
+      Then { assert File.exist? deploy_key }
     end
 
-    describe "adds /config/deploy.key to .gitignore" do
+    describe "without previously saved deploy.key" do
 
-      Then { assert_code '.gitignore', '/config/deploy.key' }
+      Given { File.delete deploy_key }
+      Given { subject.save_deploy_key }
+
+      Then { assert File.exist? deploy_key }
     end
   end
-
-  Given { ENV["DEPLOY_KEY"] = "YkvlQHABzmPH+u9BHZgggA==" }
 
   describe "#get_deploy_key" do
 
+    describe "with 'config/deploy.key'" do
 
-    Given { subject.generate_deploy_key }
-    Given { ENV["DEPLOY_KEY"] = nil }
-    Given(:actual) { Base64.encode64(subject.get_deploy_key) }
-    Given(:expected) { File.read('config/deploy.key') }
+      Given { subject.get_deploy_key }
 
-    Then { actual.must_equal expected }
+      Then { assert File.exist? 'config/deploy.key' }
+    end
+
+    describe "without 'config/deploy.key'" do
+
+      describe "without ENV['DEPLOY_KEY']" do
+
+        Given { File.delete 'config/deploy.key' }
+        Given { ENV['DEPLOY_KEY'] = nil }
+        Given { subject.get_deploy_key }
+
+        Then { assert File.exist? 'config/deploy.key' }
+      end
+
+      describe "with ENV['DEPLOY_KEY'] set" do
+
+        Given { ENV['DEPLOY_KEY'] = "some-new-deploy-password" }
+        Given { subject.get_deploy_key }
+        Given(:pass_phrase) { subject.instance_variable_get("@pass_phrase") }
+
+        Then { assert_equal pass_phrase, "some-new-deploy-password" }
+      end
+    end
   end
+
+  describe "#read_deploy_key" do
+
+    Given { subject.save_deploy_key }
+    Given(:new_key) { subject.instance_variable_get("@new_key")}
+
+    Then { subject.read_deploy_key.must_equal new_key}
+  end
+
+  Given(:env_file)        { '.env/backup.env' }
+  Given(:nested_env_file) { '.env/development/backup.env'}
 
   describe "#encrypt" do
 
-    Given { subject.encrypt('.env/circle.env') }
+    Given(:env_enc_file) { '.env/backup.env.enc' }
+    Given { subject.encrypt env_file }
 
-    Then { assert File.exist?('.env/circle.env.enc')}
+    Then { assert File.exist? env_enc_file }
 
     describe "#decrypt" do
 
-      Given { FileUtils.copy file1, file2 }
-      Given { assert File.exist? '.env/circle.env.enc'}
-      Given { subject.decrypt '.env/circle.env.enc' }
-      Then { assert File.exist? '.env/circle.env' }
+      Given { File.delete env_file}
+      Given { subject.decrypt env_enc_file }
+      Given(:actual) { File.read env_file }
+      Given(:expected) { File.read '.env/circle.env' }
+
+      Then { assert File.exist? env_file }
+      And { assert_equal actual, expected }
     end
   end
 
@@ -60,86 +89,48 @@ describe Handsomefencer::Environment::Crypto do
 
     describe "default" do
 
-      Given(:expected_files) { subject.source_files('.env') }
+      Given(:env_files) { subject.source_files('.env') }
 
-      Then { expected_files.must_include file1 }
-      And  { expected_files.must_include file2 }
+      Then { env_files.must_include env_file }
+      And  { env_files.must_include nested_env_file }
     end
 
     describe "with specified directory" do
 
-      Given(:expected_files) { subject.source_files('.env', '.env') }
+      Given(:env_files) { subject.source_files('.env', '.env') }
 
-      Then { expected_files.must_include file1 }
-      And  { expected_files.must_include file2 }
+      Then { env_files.must_include env_file }
+      And  { env_files.must_include nested_env_file }
     end
   end
 
-  describe "default" do
+  describe "#obfuscate()" do
 
-    describe "#obfuscate()" do
+    Given { subject.obfuscate }
 
-      Given { subject.obfuscate }
-
-      Then { assert File.exist? file1 + '.enc' }
-      And  { assert File.exist? file2 + '.enc' }
-      And  { assert File.exist? file3 + '.enc' }
-
-      describe "#expose" do
-
-        Given { subject.expose }
-
-        # Then { assert File.exist? file1 }
-        # And  { assert File.exist? file2 }
-        # And  { assert File.exist? file3 }
-      end
-    end
+    Then { assert File.exist? env_file + '.enc' }
+    And  { assert File.exist? nested_env_file + '.enc' }
   end
 
-  describe "specified directory" do
+  describe "#expose" do
 
-    describe "#obfuscate()" do
+    Given { subject.obfuscate }
+    Given { File.delete env_file }
+    Given { File.delete nested_env_file }
+    Given { subject.expose }
 
-      Given(:dir) { 'test/handsomefencer/dummy/local/'}
-      Given(:file4) { dir + file1 }
-      Given(:file5) { dir + file2 }
-      Given(:file6) { dir + file3 }
-      Given(:file7) { file4 + '.enc' }
-      Given(:file8) { file5 + '.enc' }
-      Given(:file9) { file6 + '.enc' }
-
-      Given { subject.obfuscate(dir + '/.env') }
-
-      Then { assert File.exist? file4 }
-      And  { assert File.exist? file5 }
-      And  { assert File.exist? file6 }
-
-      describe "#expose" do
-
-        Given { subject.obfuscate(dir + '/.env') }
-
-        Then { assert File.exist? file7 }
-      end
-    end
+    Then { assert File.exist? env_file }
+    And  { assert File.exist? nested_env_file }
   end
 
   Minitest.after_run do
-
     samples = [
       '.env/circle.env',
       '.env/backup.env',
-      '.env/development/backup.env',
-      'test/handsomefencer/dummy/local/.env/circle.env',
-      'test/handsomefencer/dummy/local/.env/backup.env',
-      'test/handsomefencer/dummy/local/.env/development/backup.env'
+      '.env/development/backup.env'
     ]
     samples.each do |file|
       FileUtils.copy('sourcefiles/circle.env', file)
     end
-    File.delete('config/deploy.key')
-    # system 'git checkout .gitignore'
-    # system 'git checkout config/deploy.key'
-
-
   end
 end
