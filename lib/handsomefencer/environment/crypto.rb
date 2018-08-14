d322243507091be7e2fe2721fcb318389c226b0c
schadenfred <fred.schoeneman@gmail.com>
2018-08-13 21:48:59 -0700

    blah

--- lib/handsomefencer/environment/crypto.rb
+++ lib/handsomefencer/environment/crypto.rb
@@ -3,61 +3,45 @@ require 'base64'
 
 class Handsomefencer::Environment::Crypto
 
-  def dkfile
-    "config/deploy.key"
+  def initialize
+    @cipher = OpenSSL::Cipher.new 'AES-128-CBC'
+    @salt = '8 octets'
+    get_deploy_key
   end
 
-  def initialize
-    @cipher = OpenSSL::Cipher::AES.new(128, :CBC)
+  def get_deploy_key
+    if ENV['DEPLOY_KEY'].nil?
+      @pass_phrase = read_deploy_key.nil? ? save_deploy_key : read_deploy_key
+    else
+      @pass_phrase = ENV['DEPLOY_KEY']
+    end
+  end
+
+  def save_deploy_key
+    @new_key = @cipher.random_key
+    write_to_file Base64.encode64(@new_key), dkfile
+    unless File.read('.gitignore').match dkfile
+      open('.gitignore', 'a') { |f| f << "/" + dkfile }
+    end
+    read_deploy_key
+  end
+
+  def read_deploy_key
+    File.exist?(dkfile) ? Base64.decode64(File.read dkfile) : nil
   end
 
   def encrypt(file)
-    @cipher.encrypt
-    @key = @cipher.random_key
+    @cipher.encrypt.pkcs5_keyivgen @pass_phrase, @salt
     encrypted = @cipher.update(File.read file) + @cipher.final
-    write_to_file Base64.encode64(@key), dkfile
     write_to_file(Base64.encode64(encrypted), file + '.enc')
   end
 
   def decrypt(file)
-    encoded = File.read file
-    encrypted = Base64.decode64 encoded
-    @decipher = OpenSSL::Cipher::AES.new(128, :CBC)
-    @decipher.decrypt
-    @key = Base64.decode64 File.read(dkfile)
-    @decipher.key = @key
-
-    decrypted = @decipher.update(encrypted) + @decipher.final
+    encrypted = Base64.decode64 File.read(file)
+    @cipher.decrypt.pkcs5_keyivgen @pass_phrase, @salt
+    decrypted = @cipher.update(encrypted) + @cipher.final
     decrypted_file = file.split('.enc').first
     write_to_file decrypted, decrypted_file
-  end
-
-  def generate_deploy_key
-    key = Base64.encode64(@cipher.random_key)
-    write_to_file(key, dkfile)
-    unless File.read('.gitignore').match dkfile
-      open('.gitignore', 'a') { |f| f << "/" + dkfile }
-    end
-    get_deploy_key
-  end
-
-  def write_to_file(data, filename)
-    open filename, "w" do |io|
-      io.write data
-    end
-  end
-
-  def get_deploy_key
-
-    file = 'config/deploy.key'
-    if File.exist?(file)
-      encoded = File.read(file)
-    else
-
-      # ENV['DEPLOY_KEY'].nil?
-      generate_deploy_key
-    end
-    decoded = Base64.decode64(encoded)
   end
 
   def source_files(directory=nil, extension=nil)
@@ -68,19 +52,24 @@ class Handsomefencer::Environment::Crypto
   def obfuscate(directory=nil, extension=nil)
     extension = extension || '.env'
     directory = directory || '.env'
-    files = source_files(directory, extension)
-
-    files.each do |file|
-      encrypt(file)
-    end
+    source_files(directory, extension).each { |file| encrypt file }
   end
 
   def expose(directory=nil, extension=nil)
     extension = extension || '.env.enc'
     directory = directory || '.env'
-    files = source_files(directory, extension)
-    files.each do |file|
-      decrypt(file)
-    end
+    source_files(directory, extension).each { |file| decrypt(file) }
   end
+
+
+  private
+
+    def dkfile
+      "config/deploy.key"
+    end
+
+    def write_to_file(data, filename)
+      open(filename, "w") { |io| io.write data }
+    end
+
 end
